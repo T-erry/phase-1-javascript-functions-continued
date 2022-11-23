c4c2e6efb94f29bfd305b6b88b6546d11abf251f
T-erry <terrywachuka10@gmail.com>
2022-11-23 11:34:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+      function saturdayFun(activity = "roller-skate"){
+        return `This Saturday, I want to ${activity}!`;
+      }
+      
+      function mondayWork(activity = "go to the office"){
+      return `This Monday, I will ${activity}.`;
+      }
+      mondayWork(" I will work from home.");
+
+      const wrapAdjective = (paramOut = "*") => {
+        return function (paramIn = "special") {
+            return (`You are ${paramOut}${paramIn}${paramOut}!`)
+        }
+    }
+    wrapAdjective("||")("a dedicated programmer")
